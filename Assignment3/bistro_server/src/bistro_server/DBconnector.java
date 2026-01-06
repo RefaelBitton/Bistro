7392547b94e95b81d8f008947c833ea7c4512969
Refael Bitton <refaelbit1@gmail.com>
2026-01-06 12:00:16 +0200

    changed sql connector pass

--- Assignment3/bistro_server/src/bistro_server/DBconnector.java
+++ Assignment3/bistro_server/src/bistro_server/DBconnector.java
@@ -51,7 +51,7 @@ public class DBconnector {
     public DBconnector(){
         try //connect DB
         {
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bistro", "root", "");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bistro", "root", "123456789");
         	//conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bistro?allowLoadLocalInfile=true&serverTimezone=Asia/Jerusalem&useSSL=false", "root", "Hodvak123!");
             System.out.println("SQL connection succeeded");
             f = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
