570377fabf50bf74b0a1b9150487e88112ec0369
HodVakrat <vakrat.hod@gmail.com>
2026-01-06 20:26:37 +0200

    fix bugs

--- Assignment3/bistro_client/src/boundry/ChangeDetailsController.java
+++ Assignment3/bistro_client/src/boundry/ChangeDetailsController.java
@@ -46,27 +46,50 @@ public class ChangeDetailsController implements IController {
 			ClientUI.console.switchScreen(this, event, "/boundry/WorkerScreen.fxml", user);
 		}
     }
-
+    
     @FXML
     void onUpdateClick(ActionEvent event) {
+    	boolean emailException = false;
+    	boolean phoneException = false;
     	String query = "UPDATE `user` SET";
     	if(!fullNameTxt.getText().isEmpty()) {
 			query += " full_name = '" + fullNameTxt.getText() + "',";
 		}
     	if(!emailTxt.getText().isEmpty()) {
-   			query += " email = '" + emailTxt.getText() + "',";
+    		if(!isValidEmail(emailTxt.getText()))
+    			emailException = true;
+    		else
+    			query += " email = '" + emailTxt.getText() + "',";
     	}
     	if(!phoneNumberTxt.getText().isEmpty()) {
-    		query += " phone_number = '" + phoneNumberTxt.getText() + "',";
+    		if(phoneNumberTxt.getText().length() != 10)
+    			phoneException = true;
+    		else
+    			query += " phone_number = '" + phoneNumberTxt.getText() + "',";
     	}
     	if(!userNameTxt.getText().isEmpty()) {
 			query += " username = '" + userNameTxt.getText() + "',";
 		}
-    	query = query.substring(0, query.length() - 1); // remove last comma
-    	query += " WHERE subscriber_id = " + user.getSubscriberID();
-    	UpdateDetailsRequest req = new UpdateDetailsRequest(query);
-    	ClientUI.console.accept(req);
-    	
+    	if(emailException) {
+			Alert alert = new Alert(AlertType.ERROR);
+    		alert.setTitle("Error Occurred");
+    		alert.setHeaderText("Input Validation Failed");
+    		alert.setContentText("Please enter valid email");
+    		alert.showAndWait();
+    	}
+    	if(phoneException) {
+			Alert alert = new Alert(AlertType.ERROR);
+    		alert.setTitle("Error Occurred");
+    		alert.setHeaderText("Input Validation Failed");
+    		alert.setContentText("Please enter valid phone number with 10 digits");
+    		alert.showAndWait();
+    	}
+    	if((!emailException) && (!phoneException)) {
+        	query = query.substring(0, query.length() - 1); // remove last comma
+        	query += " WHERE subscriber_id = " + user.getSubscriberID();
+        	UpdateDetailsRequest req = new UpdateDetailsRequest(query);
+        	ClientUI.console.accept(req);
+    	}   	
     }
 
 	@Override
@@ -82,6 +105,10 @@ public class ChangeDetailsController implements IController {
 	@Override
 	public void setUser(User user) {
 		this.user = (Subscriber) user;
+	}
+	
+	public boolean isValidEmail(String email) {
+	    return email != null && email.matches("^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$");
 	}
 
 }
