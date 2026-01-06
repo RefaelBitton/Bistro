a1e061aa91c9a509e4908ef3b946b46079611597
HodVakrat <vakrat.hod@gmail.com>
2026-01-06 11:33:10 +0200

    update sql tables

--- bistro_order.sql
+++ bistro_order.sql
@@ -31,6 +31,9 @@ CREATE TABLE `order` (
   `date_of_placing_order` date DEFAULT NULL,
   `contact` varchar(45) DEFAULT NULL,
   `status` enum('OPEN','CLOSED','CANCELLED','WAITING') DEFAULT NULL,
+  `actual_arrival` datetime DEFAULT NULL,
+  `seated_time` datetime DEFAULT NULL,
+  `leave_time` datetime DEFAULT NULL,
   PRIMARY KEY (`order_number`),
   UNIQUE KEY `order_datetime_UNIQUE` (`order_datetime`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -42,7 +45,7 @@ CREATE TABLE `order` (
 
 LOCK TABLES `order` WRITE;
 /*!40000 ALTER TABLE `order` DISABLE KEYS */;
-INSERT INTO `order` VALUES (1,'2025-12-25 13:00:00',4,1,NULL,'2025-12-24','0506421020','CANCELLED'),(2,'2025-12-25 13:30:00',4,2,123,'2025-12-24','hav@gmail.com','CANCELLED'),(3,'2026-01-01 21:30:00',2,1003,123,'2025-12-31','hav@gmail.com','CANCELLED'),(4,'2026-01-10 08:00:00',2,1004,123,'2026-01-05','hav@gmail.com','OPEN');
+INSERT INTO `order` VALUES (1,'2025-12-25 13:00:00',4,1,NULL,'2025-12-24','0506421020','CANCELLED',NULL,NULL,NULL),(2,'2025-12-25 13:30:00',4,2,123,'2025-12-24','hav@gmail.com','CANCELLED',NULL,NULL,NULL),(3,'2026-01-01 21:30:00',2,1003,123,'2025-12-31','hav@gmail.com','CANCELLED',NULL,NULL,NULL),(4,'2026-01-10 08:00:00',2,1004,123,'2026-01-05','hav@gmail.com','OPEN',NULL,NULL,NULL);
 /*!40000 ALTER TABLE `order` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -55,4 +58,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-01-05 21:02:29
+-- Dump completed on 2026-01-06 11:33:01
