fe034a891a84e58c85c8c8a2ee98f0df7b9dd9f5
Dean Zarhi <deanza12344@gmail.com>
2026-01-09 12:46:04 +0200

    Fixed UI bugs

--- Assignment3/bistro_server/src/bistro_server/DBconnector.java
+++ Assignment3/bistro_server/src/bistro_server/DBconnector.java
@@ -55,7 +55,7 @@ public class DBconnector {
         try //connect DB
         {
 
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bistro", "root", "123456789");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bistro", "root", "");
         	//conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bistro?allowLoadLocalInfile=true&serverTimezone=Asia/Jerusalem&useSSL=false", "root", "Hodvak123!");
 
             System.out.println("SQL connection succeeded");
